bd2aa725064632321236c87366357e0191adffb0
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-05-09 23:16:40 +0100

    created test file for task: 4. Create BaseModel from dictionary

---
+++ tests/test_model_from_dict.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+    This module contains a class <TestModelFromDict> that tests \
+    the create model from dictionary functionality of the magic method \
+    __init__ in BaseModel()
+"""
+
+import unittest
+
+
+class TestModelFromDict(unittest.TestCase):
+    """
+        subclass of unittest to test that the test model \
+        from dict works
+    """
+
+    def test_object_creation(self):
+        pass
