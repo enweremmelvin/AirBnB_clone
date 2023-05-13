f1684fe3f768f07e19948765c152167e818c6988
bush-da <danielsevani12@gmail.com>
2023-05-13 12:43:25 -0700

    unit tests for class BaseModel methods and attributes

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -4,7 +4,8 @@
     This module contains a class to test \
     the base model class
 """
-
+import os
+import time
 import uuid
 import unittest
 from datetime import datetime
@@ -28,12 +29,21 @@ class TestBaseModel(unittest.TestCase):
         self.assertNotEqual(class_instance.created_at, None)
         self.assertNotEqual(class_instance.updated_at, None)
 
+    def test_noarg_init(self):
+        self.assertEqual(BaseModel, type(BaseModel()))
+
+    def test_created_at_is_datetime(self):
+        self.assertEqual(datetime, type(BaseModel().created_at))
+
+    def test_id_is_str(self):
+        self.assertEqual(str, type(BaseModel().id))
+
     def test_id_randomness(self):
         """
             test to confirm that id values are random
         """
 
-        new_uid = uuid.uuid4()
+        new_uid = BaseModel()
         class_instance = BaseModel()
 
         self.assertNotEqual(new_uid, class_instance.id)
@@ -62,3 +72,30 @@ class TestBaseModel(unittest.TestCase):
         for value in attr_list:
             check = value in dict_val
             self.assertEqual(check, True)
+
+    def test_to_dict_contains_added_attributes(self):
+        class_instance = BaseModel()
+        class_instance.name = "ALX"
+        class_instance.number = 21
+        self.assertIn("name", class_instance.to_dict())
+        self.assertIn("number", class_instance.to_dict())
+
+    def test_to_dict_datetime_attributes(self):
+        class_instance = BaseModel()
+        class_dict = class_instance.to_dict()
+        self.assertEqual(str, type(class_dict["created_at"]))
+        self.assertEqual(str, type(class_dict["updated_at"]))
+
+    def test_save_one(self):
+        class_instance = BaseModel()
+        time.sleep(0.03)
+        time_of_first_update = class_instance.updated_at
+        class_instance.save()
+        self.assertLess(time_of_first_update, class_instance.updated_at)
+
+    def test_save_updates_file(self):
+        class_instance = BaseModel()
+        class_instance.save()
+        class_instance_id = "BaseModel." + class_instance.id
+        with open("objects.json", "r") as f:
+            self.assertIn(class_instance_id, f.read())
